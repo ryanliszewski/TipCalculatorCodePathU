8ffa169b2b58f52d416e9bdcfa2eae58601e9c32
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-01 15:57:48 -0800

    Removed segmentedcontrol code

--- iTipDemo/AppDelegate.swift
+++ iTipDemo/AppDelegate.swift
@@ -17,10 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
 	
-		let font = UIFont(name: "HelveticaNeue", size: 16)!
-		
-		UISegmentedControl.appearance().setTitleTextAttributes([NSAttributedStringKey.font: font],
-																						for: .normal)
     return true
   }
 
