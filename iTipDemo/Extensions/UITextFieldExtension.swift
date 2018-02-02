1fbdeb4ee1fb5673b58651b41f72a35b03efb762
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-02 10:35:28 -0800

    Added placeholder color extension

--- iTipDemo/Extensions/UITextFieldExtension.swift
+++ iTipDemo/Extensions/UITextFieldExtension.swift
@@ -11,14 +11,14 @@
 import Foundation
 import UIKit
 
-//extension UITextField {
-//	@IBInspectable var placeHolderColor: UIColor? {
-//		get {
-//			return self.placeHolderColor
-//		}
-//		set {
-//			self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: newValue!])
-//		}
-//	}
-//}
+extension UITextField {
+	@IBInspectable var placeHolderColor: UIColor? {
+		get {
+			return self.placeHolderColor
+		}
+		set {
+			self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: newValue!])
+		}
+	}
+}
 
